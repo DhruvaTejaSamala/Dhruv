b0c5fb9a792f75e588238b30ec012b92a9ee1c9c
DHRUVA TEJA SAMALA <111600527+DhruvaTejaSamala@users.noreply.github.com>
2023-05-22 14:51:53 -0500

    Initial Commit

---
+++ Dhruv/DhruvApp.swift
@@ -0,0 +1,17 @@
+//
+//  DhruvApp.swift
+//  Dhruv
+//
+//  Created by Samala,Dhruva Teja on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DhruvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
